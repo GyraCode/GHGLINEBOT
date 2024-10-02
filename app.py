a26623804fcaaf2e5fccd92c00b5ef78bb6549c7
GyRA <85630123+GyraCode@users.noreply.github.com>
2024-10-02 20:02:17 +0800

    push4

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # 設置 MongoDB 連接
-client = MongoClient("mongodb+srv://x513465:<db_password>@cluster0.ierkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://x513465:1KdJi9XRKfysuTes@cluster0.ierkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client['Cluster0']  # 選擇數據庫名稱
 messages_collection = db['messages']  # 選擇集合名稱（相當於 SQL 的表）
 
